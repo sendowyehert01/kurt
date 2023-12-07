0020d010ec03456a729ce95108b5f21e37b44c19
sendowyehert01 <johnjeffelmadula@gmail.com>
2023-12-07 22:49:10 +0800

    project kurt

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+/**
+ * Created by vagrant on 8/23/16.
+ */
+/**
+ * Created by vagrant on 8/23/16.
+ */
+/**
+ * Created by vagrant on 8/23/16.
+ */
+/**
+ * Created by vagrant on 8/10/16.
+ */
+/**
+ * Created by vagrant on 8/10/16.
+ */
+/**
+ * Created by vagrant on 8/10/16.
+ */
+///<reference path="vendingMachine.ts"/>
+var machine = new VendingMachine();
+machine.size = VendingMachineSize.medium;
+ko.applyBindings(machine);
+//# sourceMappingURL=app.js.map
